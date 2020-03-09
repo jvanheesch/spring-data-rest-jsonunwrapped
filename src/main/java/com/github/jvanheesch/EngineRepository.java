de62073e136b8fa0a83d51ceeee8f58d6e26d64f
jvanheesch <joris.vanheesch@ae.be>
2020-03-09 21:08:56 +0100

    Fixed EngineRepository.

--- src/main/java/com/github/jvanheesch/EngineRepository.java
+++ src/main/java/com/github/jvanheesch/EngineRepository.java
@@ -4,5 +4,5 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface EngineRepository extends CrudRepository<Car, Long> {
+public interface EngineRepository extends CrudRepository<Engine, Long> {
 }
